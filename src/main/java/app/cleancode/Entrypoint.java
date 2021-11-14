5719f00d475a6fdd7992b65137a248fbefae00aa
Jett Thompson <jett.thompson.59@gmail.com>
2021-11-14 11:00:51 +1100

    made the program read a line of input and log it

--- src/main/java/app/cleancode/Entrypoint.java
+++ src/main/java/app/cleancode/Entrypoint.java
@@ -1,5 +1,7 @@
 package app.cleancode;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -7,12 +9,17 @@ import java.util.logging.Logger;
 public class Entrypoint {
     private static final Logger logger = Logger.getLogger(Entrypoint.class.getName());
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Arrays.stream(Logger.getLogger("").getHandlers())
                 .forEach(handler -> handler.setLevel(Level.ALL));
         Logger packageLogger = logger.getParent();
         packageLogger.setLevel(Level.FINE);
         logger.info("Setting up");
         logger.fine(String.format("Found %d nouns", WordLists.nouns.size()));
+        logger.info("Setup complete");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Please type a sentence: ");
+        String sentence = reader.readLine();
+        logger.fine("Received sentence: " + sentence);
     }
 }
